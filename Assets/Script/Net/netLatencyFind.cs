59454e5a267612f9bd79cec66ff420900f8e9634
yoksy-dot <c501506019@tokushima-u.ac.jp>
2017-04-09 20:59:42 +0900

    キャラ同期完了

---
+++ Assets/Script/Net/netLatencyFind.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class netLatencyFind : NetworkBehaviour
+{
+    
+    
+    private NetworkClient nClient;
+	private int latency; //遅延時間
+	private Text latencyText; //遅延時間表示用テキスト
+
+	// Use this for initialization
+	void Start () {
+		nClient = GameObject.Find("Starter").GetComponent<NetworkManager>().client;
+		latencyText = GameObject.Find("Latency Text").GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        ShowLatency();
+
+    }
+
+    void ShowLatency()
+    {
+        if (isLocalPlayer)
+        {
+            //latencyを取得
+            latency = nClient.GetRTT();
+            //latencyを表示
+            latencyText.text = latency.ToString();
+        }
+    }
+}
